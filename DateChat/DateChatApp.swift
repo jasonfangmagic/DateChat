4a2ce7ff83d2fcc7c46877a5f256f2b2d5be5e8d
jasonfangmagic <jasonfangmagic@gmail.com>
2024-06-15 21:50:43 -0400

    Initial Commit

---
+++ DateChat/DateChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateChatApp.swift
+//  DateChat
+//
+//  Created by Jason Fang on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct DateChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
